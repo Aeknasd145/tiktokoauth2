e027040554d4b6eabbffec4e7efa1aa38c56f93b
Abbas Eren Kılıç <67686692+Aeknasd145@users.noreply.github.com>
2024-04-24 23:21:27 +0300

    Update loginWithTikTok.php
    added callback part of the login with tiktok

--- examples/loginWithTikTok.php
+++ examples/loginWithTikTok.php
@@ -9,5 +9,20 @@
 
     $tikTok = new TikTok($tikTokClientKey, $tikTokClientSecret, $tikTokCallbackUrl);
     $tikTokAuthUrl = $tikTok->getAuthUrl();
+
+    // Lets say you are using the same page as callback
+    if($_GET){
+        $code = $_GET['code'];
+        $accessTokenData = $tiktok->fetchAccessToken($code);
+
+        // All the data returned, probably you will only need access token, expires in, refresh token, refresh expires in
+    	$accessToken = $accessTokenData['access_token'];
+    	$expiresIn = $accessTokenData['expires_in'];
+    	$openId = $accessTokenData['open_id'];
+    	$refreshExpiresIn = $accessTokenData['refresh_expires_in'];
+    	$refreshToken = $accessTokenData['refresh_token'];
+    	$scope = $accessTokenData['scope'];
+    	$tokenType = $accessTokenData['token_type'];
+    }
 ?>
 <a href="<?= $tikTokAuthUrl; ?>">Login with TikTok</a>
